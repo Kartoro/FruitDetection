37608469aa6e068b0585a50f7832401ad1cf66a2
Harry <harryhpxs@gmail.com>
2019-05-04 14:16:30 +1000

    Update

--- app/server.py
+++ app/server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__, template_folder='html')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def parse_upload():
+    img = request.files.get('imagefile', '')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
