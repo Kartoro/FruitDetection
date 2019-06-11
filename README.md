80ad8ee80d2b1f3d0463cf3330b86a9abbed6ce9
shaoxim <shaoxim@student.unimelb.edu.au>
2019-06-11 23:00:10 +1000

    Wrap up

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 > A web application that identifies oranges and other fruits based on Faster R-CNN model using TensorFlow object detection API.
 
+
+### Demo
+[App Demo Video](https://www.youtube.com/watch?v=JXQLtP5A_uI)
 ## Getting Started
 
 ### Prerequisites
@@ -50,11 +53,25 @@ YOUR_PROJECT_DIR
 $ cd DIR_TO_YOUR_PROJECT/models/research/object_detection
 $ python3 server.py
 ```
+## Usage
+ - Open a web browser and visit `http://localhost:5000/`
+ - Click orange `camera` button to take a photo via camera or from local gallery
+ - Upload it by clicking blue `upload` button
+ - Wait for the result to be displayed
+
+## External Libraries
+ - [TensorFlow](https://www.tensorflow.org/)
+ - [jQuery 3.4.1](https://jquery.com/)
+ - [JavaScript Load Image](https://github.com/blueimp/JavaScript-Load-Image)
+ - [Flask](http://flask.pocoo.org/)
+ - [Google Fonts](https://fonts.google.com/)
+ - [Pure CSS Loaders](https://loading.io/css/)
+
 
 ## Note
-This app was developed for COMP90055 Computing Project.
+This app was developed for COMP90055 Computing Project supervised by Prof. Richard Sinnott.
 
 Authors of the project are:
-  - Huaqing Yu
-  - Shining Song
-  - Shaoxi Ma
+ - Huaqing Yu
+ - Shining Song
+ - Shaoxi Ma
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,7 +1,13 @@
+// Funtions to control web front-end elements,
+// process input imamge, send to server,
+// and get labelled image and count response.
+
 (function() {
     var takeBtn = document.querySelector("#takeBtn");
     var photoFrame = document.querySelector("#photoFrame");
     $("#loading").hide();
+
+    // Click camera button to input image
     takeBtn.addEventListener("click", function() {
         var inputImage = document.querySelector("#inputImage");
         inputImage.click();
@@ -10,6 +16,7 @@
             while (photoFrame.firstChild) {
                 photoFrame.removeChild(photoFrame.firstChild);
             }
+            // Process input image
             var loadingImage = loadImage(
                 e.target.files[0],
                 function(img) {
@@ -28,6 +35,7 @@
         };
     }, false);
 
+    // Click upload button to send image
     document.querySelector("#upBtn").addEventListener("click", function() {
         var canvas = photoFrame.querySelector("canvas");
         if (canvas == null) {
@@ -37,6 +45,7 @@
         sendFile(canvas);
     });
 
+    // Convert canvas to blob
     function processImage(canvas) {
         return new Promise(function(resolve, reject) {
             canvas.toBlob(function(blob) {
@@ -47,6 +56,7 @@
         });
     }
 
+    // Ajax send image to server
     function sendFile(canvas) {
         processImage(canvas).then(function(blob) {
             var formData = new FormData();
